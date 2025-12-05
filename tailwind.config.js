8aeb24f1ed22127eb0ac8173049ab8633af88f33
Nikita Ishimbaev <47396666+ishimbaevnv@users.noreply.github.com>
2025-12-05 15:59:22 +0500

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  // Список файлов, в которых Tailwind будет искать классы для сборки
+export default {
   content: [
-    './*.html', 
-    './src/**/*.{js,ts,jsx,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
