2b5e0836473b83b8f7432c2ae75f4e14f9f5745e
Nikita Ishimbaev <47396666+ishimbaevnv@users.noreply.github.com>
2025-12-05 16:25:17 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/altera/',
-});
+  plugins: [react()],
+  base: "/altera/", // <-- ВАЖНО: Добавьте эту строку!
+})
